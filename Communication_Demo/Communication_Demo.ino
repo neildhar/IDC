eb84a098aa841debf14a76c4616db4523455b567
Neil Dhar <pranks16@gmail.com>
2017-03-28 22:43:58 -0400

    Stylistic improvements, RFID Demo ready

--- Communication_Demo/Communication_Demo.ino
+++ Communication_Demo/Communication_Demo.ino
@@ -1,23 +1,27 @@
+#define gLEDPin 8
+#define rLEDPin 9
+#define buttonPin 6
+
 void setup() {
   Serial3.begin(9600);
   Serial.begin(9600);
-  pinMode(8, OUTPUT); /*set send LED pin mode*/
-  pinMode(9, OUTPUT);   /*set receive LED pin mode*/
+  pinMode(gLEDPin, OUTPUT); /*set send LED pin mode*/
+  pinMode(rLEDPin, OUTPUT);   /*set receive LED pin mode*/
   pinMode(6, INPUT); /* set button pin mode*/
 }
 
-long lastRec=0; /*time of last received byte*/
+long lastXBeeRec=0; /*time of last received XBee byte*/
 
 void loop() {
-  if(Serial3.available() && !digitalRead(6)){ /*if XBee data is available and button is not pressed, start reading from serial buffer*/
+  if(Serial3.available() && !digitalRead(buttonPin)){ /*if XBee data is available and button is not pressed, start reading from serial buffer*/
     Serial.println((char)Serial3.read()); /*print received byte*/
-    digitalWrite(9, HIGH); /*turn on receive LED*/
-    lastRec = millis(); /* update time of last received byte*/
+    digitalWrite(rLEDPin, HIGH); /*turn on receive LED*/
+    lastXBeeRec = millis(); /* update time of last received byte*/
   }
-  else if(digitalRead(6)){ /*if button is pressed, start sending*/
-    digitalWrite(8, HIGH); /*turn on send LED*/
+  else if(digitalRead(buttonPin)){ /*if button is pressed, start sending*/
+    digitalWrite(gLEDPin, HIGH); /*turn on send LED*/
     Serial3.print('M'); /*send a byte*/
   }
- if(!digitalRead(6)) digitalWrite(8,LOW); /*if the button is not pressed, turn off the send LED*/
- if(!Serial3.available()&&millis()-lastRec>50) digitalWrite(9, LOW); /*ensure LED is on for at least 50ms to ensure it visibly turns on and to give time for the next byte*/
+ if(!digitalRead(buttonPin)) digitalWrite(gLEDPin,LOW); /*if the button is not pressed, turn off the send LED*/
+ if(!Serial3.available()&&millis()-lastXBeeRec>50) digitalWrite(rLEDPin, LOW); /*ensure LED is on for at least 50ms to ensure it visibly turns on and to give time for the next byte*/
 } 
